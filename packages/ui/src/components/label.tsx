4b276ec2a42909acc721460f5f2f69a2eb1ebc8a
Mnigos <makowskiigor@gmail.com>
2026-05-12 20:11:29 +0200

    feat(ui): allow label htmlFor props

--- packages/ui/src/components/label.tsx
+++ packages/ui/src/components/label.tsx
@@ -1,15 +1,27 @@
-import { Field } from '@base-ui/react/field'
 import { cn } from '@repo/ui/utils'
+import type { ComponentPropsWithoutRef } from 'react'
 
-export function Label({ className, ...props }: Readonly<Field.Label.Props>) {
+interface LabelProps extends ComponentPropsWithoutRef<'label'> {
+	htmlFor: string
+}
+
+export function Label({
+	children,
+	className,
+	htmlFor,
+	...props
+}: Readonly<LabelProps>) {
 	return (
-		<Field.Label
+		<label
 			className={cn(
 				'flex select-none items-center gap-2 font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50',
 				className
 			)}
 			data-slot="label"
+			htmlFor={htmlFor}
 			{...props}
-		/>
+		>
+			{children}
+		</label>
 	)
 }
