60c868fbacdfde99c21b79867a909ec295d402e1
Mnigos <makowskiigor@gmail.com>
2026-05-13 22:05:45 +0200

    fix(git-access-tokens): clear stale token state

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -2,7 +2,6 @@ export type { Auth } from './server'
 export { initAuth } from './server'
 export {
 	GIT_ACCESS_TOKEN_CONFIG_ID,
-	GIT_ACCESS_TOKEN_PERMISSION_MAPPING,
 	GIT_ACCESS_TOKEN_PERMISSIONS,
 	GIT_ACCESS_TOKEN_PREFIX,
 	type GitAccessTokenPermission,
