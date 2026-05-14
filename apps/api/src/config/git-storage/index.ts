21df837130e2b72a7a9ee4254c8268ce4fd385f4
Mnigos <makowskiigor@gmail.com>
2026-05-14 23:58:13 +0200

    refactor(api): extract git storage proto path helpers

--- apps/api/src/config/git-storage/index.ts
+++ apps/api/src/config/git-storage/index.ts
@@ -1,3 +1,4 @@
 export { TESSERA_GIT_V1_PACKAGE_NAME } from './generated/tessera/git/v1/git_storage'
 export * from './git-storage.client'
 export * from './git-storage.module'
+export * from './git-storage-proto-paths'
