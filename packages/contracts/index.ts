9bf033ad705c03954171d881bcc24af57014a719
Mnigos <makowskiigor@gmail.com>
2026-05-18 22:48:46 +0200

    feat(contracts): add gpg public key endpoints

--- packages/contracts/index.ts
+++ packages/contracts/index.ts
@@ -1,5 +1,6 @@
 export * from './src/auth.contract'
 export * from './src/git-access-tokens.contract'
+export * from './src/gpg-public-keys.contract'
 export * from './src/health.contract'
 export * from './src/organizations.contract'
 export * from './src/repositories.contract'
@@ -8,6 +9,7 @@ export * from './src/user.contract'
 
 import { authContract } from './src/auth.contract'
 import { gitAccessTokensContract } from './src/git-access-tokens.contract'
+import { gpgPublicKeysContract } from './src/gpg-public-keys.contract'
 import { healthContract } from './src/health.contract'
 import { organizationsContract } from './src/organizations.contract'
 import { repositoriesContract } from './src/repositories.contract'
@@ -17,6 +19,7 @@ import { userContract } from './src/user.contract'
 export const contract = {
 	auth: authContract,
 	gitAccessTokens: gitAccessTokensContract,
+	gpgPublicKeys: gpgPublicKeysContract,
 	health: healthContract,
 	organizations: organizationsContract,
 	repositories: repositoriesContract,
